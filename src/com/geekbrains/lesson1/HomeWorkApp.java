b5da3a0a6e8e22c67b30c4facd576cc106128b7b
Евгений <evg.denisenko@gmail.com>
2023-01-07 20:13:46 +0300

    HW1

---
+++ src/com/geekbrains/lesson1/HomeWorkApp.java
@@ -0,0 +1,49 @@
+package com.geekbrains.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        System.out.println();
+        checkSumSign();
+        System.out.println();
+        printColor();
+        System.out.println();
+        compareNumbers();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    private static void checkSumSign() {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная.");
+        } else {
+            System.out.println("Сумма отрицательная.");
+        }
+    }
+
+    private static void printColor() {
+        int value = 55;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void compareNumbers() {
+        int a = 3;
+        int b = 5;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
